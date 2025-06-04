1f333991e7870336e7b74897fed768d5d1692cd4
Ignacio Bardin <ilbardin@gmail.com>
2025-06-04 11:17:43 -0300

    ajuste config ruta login

--- src/main/java/BuenSabor/Config/SecurityConfig.java
+++ src/main/java/BuenSabor/Config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/login").permitAll()
+                        .requestMatchers("/api/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
