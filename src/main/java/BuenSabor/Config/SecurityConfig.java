218705c521cfe9bc15e5702e5de2433f82d6a0ad
Ignacio Bardin <ilbardin@gmail.com>
2025-06-05 09:35:49 -0300

    actualizar security config

--- src/main/java/BuenSabor/Config/SecurityConfig.java
+++ src/main/java/BuenSabor/Config/SecurityConfig.java
@@ -31,6 +31,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {
+                })
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login").permitAll()
