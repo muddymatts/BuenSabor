95186e7f59a646bd4464392711d5b86fda87f1c4
muddymatts <matiasebarroso@gmail.com>
2025-06-11 20:31:23 -0300

    Crear Repositorios para PedidoVenta u PedidoVentaDetalle

---
+++ src/main/java/BuenSabor/repository/PedidoVentaRepository.java
@@ -0,0 +1,7 @@
+package BuenSabor.repository;
+
+import BuenSabor.model.PedidoVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoVentaRepository extends JpaRepository<PedidoVenta, Long> {
+}
