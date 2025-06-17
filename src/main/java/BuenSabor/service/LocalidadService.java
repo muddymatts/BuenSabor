461588df0275d441efed26fb5a25119d78e631ef
Ignacio Bardin <ilbardin@gmail.com>
2025-06-17 18:36:37 -0300

    retornar localidades por orden alfabetico

--- src/main/java/BuenSabor/service/LocalidadService.java
+++ src/main/java/BuenSabor/service/LocalidadService.java
@@ -6,6 +6,7 @@ import BuenSabor.model.Localidad;
 import BuenSabor.repository.LocalidadRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -20,11 +21,15 @@ public class LocalidadService {
     }
 
     public List<Localidad> findAll() {
-        return localidadRepository.findAll();
+        List<Localidad> localidades = localidadRepository.findAll();
+        localidades.sort(Comparator.comparing(Localidad::getNombre));
+        return localidades;
     }
 
     public List<Localidad> findByProvinciaId(int provinciaId) {
-        return localidadRepository.findByProvinciaId(provinciaId);
+        List<Localidad> localidades = localidadRepository.findByProvinciaId(provinciaId);
+        localidades.sort(Comparator.comparing(Localidad::getNombre));
+        return localidades;
     }
 
     public List<LocalidadDTO> mapToDTOList(List<Localidad> localidades) {
