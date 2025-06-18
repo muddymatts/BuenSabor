21d3548a4e24f896e8564148c44071dfa98a37a6
Ignacio Bardin <ilbardin@gmail.com>
2025-06-18 13:50:30 -0300

    ajuste id auto incremental Domicilio

--- src/main/java/BuenSabor/model/Domicilio.java
+++ src/main/java/BuenSabor/model/Domicilio.java
@@ -1,9 +1,6 @@
 package BuenSabor.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
@@ -14,6 +11,7 @@ import org.hibernate.annotations.ColumnDefault;
 public class Domicilio {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String calle;
