e76cc0c0b1d7e9c523821fb09b2fcc5f892672fe
muddymatts <matiasebarroso@gmail.com>
2025-06-18 18:52:05 -0300

    cambio de anotacion @Autowired por contructor.

--- src/main/java/BuenSabor/controller/ImagenController.java
+++ src/main/java/BuenSabor/controller/ImagenController.java
@@ -1,7 +1,6 @@
 package BuenSabor.controller;
 
 import BuenSabor.service.ImagenService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
