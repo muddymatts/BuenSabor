5e886a01db341a4a3cbcccb14a1b5341fd2dc0ce
Ignacio Bardin <ilbardin@gmail.com>
2025-06-19 13:34:45 -0300

    ajustes RegistroUsuarioController

--- src/main/java/BuenSabor/controller/RegistroUsuarioController.java
+++ src/main/java/BuenSabor/controller/RegistroUsuarioController.java
@@ -3,9 +3,11 @@ package BuenSabor.controller;
 import BuenSabor.dto.request.RegistroUsuarioRequest;
 import BuenSabor.dto.response.RegistroResponse;
 import BuenSabor.service.RegistroUsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/registro-usuario")
@@ -13,7 +15,6 @@ public class RegistroUsuarioController {
 
     private final RegistroUsuarioService registroUsuarioService;
 
-    @Autowired
     public RegistroUsuarioController(RegistroUsuarioService registroUsuarioService) {
         this.registroUsuarioService = registroUsuarioService;
     }
