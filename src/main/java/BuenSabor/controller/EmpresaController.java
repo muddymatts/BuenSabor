9fe4827ba01f7048a82a5ddfec1c83e70260056a
muddymatts <matiasebarroso@gmail.com>
2025-07-09 14:34:59 -0300

    agrego metodos para listar las sucursales de una empresa.

--- src/main/java/BuenSabor/controller/EmpresaController.java
+++ src/main/java/BuenSabor/controller/EmpresaController.java
@@ -1,6 +1,7 @@
 package BuenSabor.controller;
 
 import BuenSabor.model.Empresa;
+import BuenSabor.model.SucursalEmpresa;
 import BuenSabor.service.empresa.EmpresaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,5 +24,32 @@ public class EmpresaController {
     @GetMapping("/{id}")
     public ResponseEntity<Empresa> getEmpresa(@PathVariable Long id){
         return ResponseEntity.ok(service.getEmpresa(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Empresa>> getEmpresas(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}/sucursales")
+    public ResponseEntity<Iterable<SucursalEmpresa>> getSucursales(@PathVariable Long id){
+        return ResponseEntity.ok(service.getSucursales(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Empresa> bajaEmpresa(@PathVariable Long id){
+        service.darDeBaja(Empresa.class, id);
+        return ResponseEntity.ok(service.getEmpresa(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Empresa> reestablecerEmpresa(@PathVariable Long id){
+        service.reestablecer(Empresa.class, id);
+        return ResponseEntity.ok(service.getEmpresa(id));
+    }
+
+    @PutMapping()
+    public ResponseEntity<Empresa> editarEmpresa( @RequestBody Empresa empresa){
+        return ResponseEntity.ok(service.editarEmpresa(empresa));
     }
 }
