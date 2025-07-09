3e47141c337006638a041b392577fc95d29d0172
muddymatts <matiasebarroso@gmail.com>
2025-07-09 14:34:37 -0300

    agrego metodos para listar las sucursales de una empresa.

--- src/main/java/BuenSabor/repository/sucursal/SucursalEmpresaRepository.java
+++ src/main/java/BuenSabor/repository/sucursal/SucursalEmpresaRepository.java
@@ -1,9 +1,11 @@
 package BuenSabor.repository.sucursal;
 
+import BuenSabor.model.Empresa;
 import BuenSabor.model.SucursalEmpresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SucursalEmpresaRepository extends JpaRepository<SucursalEmpresa, Long>, SucursalEmpresaRepositoryCustom {
+    Iterable<SucursalEmpresa> findSucursalEmpresaByEmpresa(Empresa referenceById);
 }
--- src/main/java/BuenSabor/service/empresa/EmpresaService.java
+++ src/main/java/BuenSabor/service/empresa/EmpresaService.java
@@ -1,17 +1,22 @@
 package BuenSabor.service.empresa;
 
 import BuenSabor.model.Empresa;
+import BuenSabor.model.SucursalEmpresa;
 import BuenSabor.repository.empresa.EmpresaRespository;
+import BuenSabor.repository.sucursal.SucursalEmpresaRepository;
 import BuenSabor.service.BajaLogicaService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmpresaService extends BajaLogicaService {
 
     private final EmpresaRespository respository;
+    private final SucursalEmpresaRepository sucursalEmpresaRepository;
 
-    public EmpresaService (EmpresaRespository repository){
+    public EmpresaService (EmpresaRespository repository, SucursalEmpresaRepository sucursalEmpresaRepository){
         this.respository = repository;
+        this.sucursalEmpresaRepository = sucursalEmpresaRepository;
     }
 
     public Empresa crear(Empresa empresa) {
@@ -20,5 +25,17 @@ public class EmpresaService extends BajaLogicaService {
 
     public Empresa getEmpresa(Long id) {
         return respository.getReferenceById(id);
+    }
+
+    public Empresa editarEmpresa(Empresa empresa) {
+        return respository.save(empresa);
+    }
+
+    public Iterable<SucursalEmpresa> getSucursales(Long id) {
+        return sucursalEmpresaRepository.findSucursalEmpresaByEmpresa(respository.getReferenceById(id));
+    }
+
+    public ResponseEntity<Iterable<Empresa>> findAll() {
+        return ResponseEntity.ok(respository.findAll());
     }
 }
