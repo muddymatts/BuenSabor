60921a7cc885f468f435e380b5ceb18e4b0e9747
Ignacio Bardin <ilbardin@gmail.com>
2025-07-15 16:45:10 -0300

    agregar clase con constantes de rutas publicas

---
+++ src/main/java/BuenSabor/config/SecurityConstants.java
@@ -0,0 +1,12 @@
+package BuenSabor.config;
+
+public class SecurityConstants {
+    public static final String[] PUBLIC_PATHS = {
+            "/api/auth/login",
+            "/api/registro-usuario",
+            "/uploads/images/**",
+            "/api/paises/**",
+            "/api/provincias/**",
+            "/api/localidades/**"
+    };
+}
