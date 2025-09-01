d85e18b866a7681a2839d19b78aed1d26179b823
muddymatts <matiasebarroso@gmail.com>
2025-08-31 21:01:34 -0300

    refactor, limpio el mapper, paso el metodo de traer la lista de categorias anidadas al modelo, CategoriaArticuloInsumor

--- src/main/java/BuenSabor/model/CategoriaArticuloInsumo.java
+++ src/main/java/BuenSabor/model/CategoriaArticuloInsumo.java
@@ -1,10 +1,13 @@
 package BuenSabor.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,5 +19,21 @@ public class CategoriaArticuloInsumo extends EntityApp {
     @ManyToOne
     @JoinColumn(name = "categoria_padre_id")
     private CategoriaArticuloInsumo categoriaPadre;
+
+    private List<String> recorrerCategorias (CategoriaArticuloInsumo categoria, List<String> categoriasAnidadas){
+
+        categoriasAnidadas.add(categoria.getDenominacion());
+
+        if (categoria.categoriaPadre == null) return categoriasAnidadas;
+
+        recorrerCategorias(categoria.getCategoriaPadre(), categoriasAnidadas);
+
+        return categoriasAnidadas;
+    }
+
+    @JsonIgnore
+    public List<String> getCategoriasAnidadas(){
+        return recorrerCategorias(this, new java.util.ArrayList<>());
+    }
 
 }
