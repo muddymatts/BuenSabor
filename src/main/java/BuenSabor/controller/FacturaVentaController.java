e15f7b59c943012ee023031d2265d9e022e2d4a5
muddymatts <matiasebarroso@gmail.com>
2025-09-06 21:42:56 -0300

    agrego metodos para generar el pdf de la factura

--- src/main/java/BuenSabor/controller/FacturaVentaController.java
+++ src/main/java/BuenSabor/controller/FacturaVentaController.java
@@ -3,26 +3,41 @@ package BuenSabor.controller;
 import BuenSabor.dto.response.ResponseDTO;
 import BuenSabor.enums.FormaPago;
 import BuenSabor.model.FacturaVenta;
+import BuenSabor.service.facturaVenta.FacturaPdfService;
 import BuenSabor.service.facturaVenta.FacturaVentaSevice;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/facturas")
 public class FacturaVentaController {
 
     private final FacturaVentaSevice facturaVentaService;
+    private final FacturaPdfService facturaPdfService;
 
-    public FacturaVentaController(FacturaVentaSevice facturaVentaService) {
+    public FacturaVentaController(FacturaVentaSevice facturaVentaService,
+                                  FacturaPdfService facturaPdfService) {
         this.facturaVentaService = facturaVentaService;
+        this.facturaPdfService = facturaPdfService;
     }
 
     @PutMapping("/facturar/{idPedido}")
-    public ResponseEntity<ResponseDTO> facturarPedido(@PathVariable Long idPedido) {
+    public ResponseEntity<ResponseDTO> facturarPedidoEfectivo(@PathVariable Long idPedido) {
         FacturaVenta factura = facturaVentaService.facturarPedido(idPedido, FormaPago.Efectivo);
         return ResponseEntity.ok(new ResponseDTO("Pedido Facturado Correctamente", factura.getId()));
+    }
+
+    @GetMapping("/{id}/pdf")
+    public ResponseEntity<byte[]> generarFacturaPdf(@PathVariable Long id) throws Exception {
+        FacturaVenta factura = facturaVentaService.getFactura(id);
+
+        byte[] pdfBytes = facturaPdfService.generarPdf(factura);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=factura-" + id + ".pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdfBytes);
     }
 }
